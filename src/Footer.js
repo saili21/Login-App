522038afb3fd7d0cbecc8ce839bc30f9b0f690df
Saili Jawale <sailijawale2101@gmail.com>
2021-12-01 11:45:48 +0530

    footer adjusted

--- src/Footer.js
+++ src/Footer.js
@@ -1,32 +1,30 @@
 import { Grid, Typography } from '@mui/material';
 import { makeStyles } from '@mui/styles';
+import { padding } from '@mui/system';
 import React from 'react';
 
-const useStyles = makeStyles((theme)=>({
+const useStyles = makeStyles((theme) => ({
     customFooter: {
         position: "fixed",
         bottom: 0,
         width: "100%",
-        height: 50,
+        height: 60,
         background: "grey",
-      }
+        paddingTop: 10,
+        paddingBottom:10
+    }
 }))
 
 function Footer(props) {
     const classes = useStyles();
     return (
         <Grid container
-            spacing={0}
-            direction="column"
-            alignItems="center"
-            justify="center"
+            justifyContent="center"
             className={classes.customFooter}>
-            <Grid item>
+            <Grid container justifyContent="center" item xs={12}>
                 <Typography>Your account for everything Autodesk</Typography>
             </Grid>
-            <Grid item>
-                <Typography><a href="/">Learn More</a></Typography>
-            </Grid>
+            <Typography><a href="/">Learn More</a></Typography>
         </Grid>
     );
 }
